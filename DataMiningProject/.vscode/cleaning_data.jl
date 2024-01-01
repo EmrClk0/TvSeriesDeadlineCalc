0456b68b35724b6b367dbc750bff47914909d032
Uğur Arpacı <73073617+ugurarpac@users.noreply.github.com>
2024-01-01 16:25:42 +0300

    Update cleaning_data.jl

--- DataMiningProject/.vscode/cleaning_data.jl
+++ DataMiningProject/.vscode/cleaning_data.jl
@@ -9,6 +9,15 @@ data = dropmissing(data)
 
 data.Format .= replace.(data.Format, "İnternet dizisi" => "Dijital")
 data.Format .= replace.(data.Format, "İnternet film serisi" => "Dijital")
+data.Format .= replace.(data.Format, "Televizyon filmi" => "TV")
+data.Format .= replace.(data.Format, "Doğaçlama;;skeçler" => "TV")
+data.Format .= replace.(data.Format, "Televizyon programı" => "TV")
+data.Format .= replace.(data.Format, "Televizyon dizisi;;programı" => "TV")
+data.Format .= replace.(data.Format, "Televizyon film serisi" => "TV")
+data.Format .= replace.(data.Format, "Televizyon dizisi" => "TV")
+data.Format .= replace.(data.Format, "TV;;Dijital" => "TV-Dijital")
+data.Format .= replace.(data.Format, "Televizyon Dizisi" => "TV")
+
 
 selected_columns = [:AD, :Format, :Tür, :Senarist, :Yönetmen, :Başrol, :Ülke, :Dili, :Sezonsayısı, :Bölümsayısı, :Yapımcı, :Gösterimsüresi, :Yapımşirketi, :Kanal, :Yayıntarihi, :Durumu]
 subset_data = select(data, selected_columns)
